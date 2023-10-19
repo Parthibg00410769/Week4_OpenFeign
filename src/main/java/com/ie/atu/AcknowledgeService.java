f778974a0c4a3e8b6767eef780f8bd84b0c0af44
Parthibg00410769 <g00410769@atu.ie>
2023-10-19 01:47:00 +0100

    part A has been amended according to the hw

--- src/main/java/com/ie/atu/AcknowledgeService.java
+++ src/main/java/com/ie/atu/AcknowledgeService.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 @Data
 @Service
 public class AcknowledgeService {
+
     public String ackMessage(UserDetails user){
         String message = "Thank you" + user.getName() + " your request has been received";
         return message;
